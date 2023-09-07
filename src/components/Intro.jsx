e05f964733cb1004d2cab0c00a740396e6aa2b6c
Sebastian <mrseb10@gmail.com>
2023-09-07 22:56:06 +0200

    Intro page

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <section className='intro-page'>
+      <h1>Quizzical</h1>
+      <p>5 simple (or not) questions.<br /> Can you answer them all?</p>
+      <button>Start quiz</button>
+    </section>
+  )
+}
