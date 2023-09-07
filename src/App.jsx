4235a697c8102b93bd5efea6786c606c1f23e5f4
Sebastian <mrseb10@gmail.com>
2023-09-07 23:12:32 +0200

    Start Quiz button changes state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Intro from './components/Intro'
+import Main from './components/Main'
 
 export default function App() {
+  const [page, setPage] = React.useState(<Intro 
+  changePage={changePage}
+  />)
+
+  function changePage() {
+    setPage(<Main />)
+  }
+
   return (
     <main>
-      <Intro />
+      {page}
     </main>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Intro() {
+export default function Intro(props) {
   return (
     <section className='intro-page'>
       <h1>Quizzical</h1>
       <p>5 simple (or not) questions.<br /> Can you answer them all?</p>
-      <button>Start quiz</button>
+      <button onClick={props.changePage}>Start quiz</button>
     </section>
   )
 }
