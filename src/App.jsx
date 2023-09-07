2884e52567d85af88f49b968d5266562802c7cd2
Sebastian <mrseb10@gmail.com>
2023-09-07 22:32:32 +0200

    Background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 export default function App() {
-  fetch("https://opentdb.com/api.php?amount=5")
-    .then(res => res.json())
-    .then(data => console.log(data))
+  return (
+    <main>
+      
+    </main>
+  )
 }
