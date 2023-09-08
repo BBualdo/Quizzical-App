c8039458713e65f99919d0f63735c8e373d96f74
Sebastian <mrseb10@gmail.com>
2023-09-08 02:31:07 +0200

    Mapping over arrays to render the page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,15 +13,17 @@ export default function Main() {
 
   return (
     <section className='main-page'>
-      <div className='question-container'>
-        <p className='question-text'>How would one say goodbye in Spanish?</p>
+      {questionsData.map((questionData, index) => (
+          <div key={index} className='question-container'>
+        <p className='question-text'>{questionData.question}</p>
         <div className='answers'>
-          <button className='answer-button'>Adiós</button>
-          <button className='answer-button'>Hola</button>
-          <button className='answer-button'>Au Revoir</button>
-          <button className='answer-button'>Salir</button>
+          {shuffleAnswers(questionData).map((answer, answerIndex) => (
+            <button key={answerIndex} className='answer-button'>{answer}
+            </button>
+          ))}
         </div>
       </div>
+      ))}
       <button className='main-button'>Check answers</button>
     </section>
   )
